efb427739bde6d2d998c04572300abc331f92f4e
idriss khattabi <49597702+drisskhattabi6@users.noreply.github.com>
2024-08-25 10:21:51 +0100

    Update settings.py

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -148,6 +148,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'drisskhattabi6@gmail.com'
-EMAIL_HOST_PASSWORD = 'rjdzbookqirvymyo'
+EMAIL_HOST_USER = 'YOUR_EMAIL_HOST'
+EMAIL_HOST_PASSWORD = 'YOUR_EMAIL_HOST_PASSWORD'
 
